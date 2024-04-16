72e3b369155230b62e63eb30086ed617e17b2185
Noith <nmontealegre@imaginacolombia.com>
2024-04-16 07:52:41 -0500

    Added new files

--- app/[locale]/rte/constant/Files.constant.ts
+++ app/[locale]/rte/constant/Files.constant.ts
@@ -3,17 +3,27 @@ import { FilesPDF } from '@/interfaces/default';
 export const FILESPDF: FilesPDF[] = [
   {
     key: 0,
-    name: 'Regimen Tributario Especial - 2021',
-    file: '/assets/rte.pdf'
+    name: 'Camara de Camercio - 2024',
+    file: '/assets/Camara_Comercio_Marzo_21_2024.pdf'
   },
   {
     key: 1,
-    name: 'Regimen Tributario Especial - 2022 - 2023',
-    file: '/assets/rte2022.pdf'
+    name: 'Estados Financieros - 2023',
+    file: '/assets/Estados_financieros_2023.pdf'
   },
   {
     key: 2,
     name: 'RUT - 2023',
     file: '/assets/Rut_Actual_2023.pdf'
+  },
+  {
+    key: 3,
+    name: 'Regimen Tributario Especial - 2021',
+    file: '/assets/rte.pdf'
+  },
+  {
+    key: 4,
+    name: 'Regimen Tributario Especial - 2022 - 2023',
+    file: '/assets/rte2022.pdf'
   }
 ];
